63b5597a6e0a2ce80c4bf08a13dfda6ffa2d4cda
shlokGupta <shlok.official01@gmail.com>
2026-01-31 23:25:39 +0530

    fix: update parameter handling in GET and DELETE routes to await promise

--- app/api/resources/[id]/route.ts
+++ app/api/resources/[id]/route.ts
@@ -2,15 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(
   request: NextRequest,
-  { params }: { params: { id: string } },
+  { params }: { params: Promise<{ id: string }> },
 ) {
-  const { id } = params;
-  console.log("THis is the id", id)
+    const { id } = await params;
+  console.log("THis is the id", id);
   try {
     // Mock: Delete resource
     return NextResponse.json({ success: true });
   } catch (error) {
-    console.log("Error inside dynamic id inside resources", error)
+    console.log("Error inside dynamic id inside resources", error);
     return NextResponse.json(
       { error: "Failed to delete resource" },
       { status: 500 },
